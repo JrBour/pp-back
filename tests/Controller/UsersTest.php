5de6b8b3c9c68ecc38ffca1e249a1ca794ff713a
JrBour <calaruni@hotmail.com>
2020-11-08 20:38:01 +0100

    test(users): Add method for check if we get correct response when we get users collection

--- tests/Controller/UsersTest.php
+++ tests/Controller/UsersTest.php
@@ -2,10 +2,20 @@
 namespace App\Tests\Controller;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
 use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
 
 class UsersTest extends ApiTestCase
 {
     use RefreshDatabaseTrait;
+
+    public function testGetCollection(): void
+    {
+        $response = static::createClient()->request('GET', '/users');
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/json');
+        $this->assertMatchesResourceCollectionJsonSchema(User::class);
+
+    }
 
 }
