665bd7c435a277fbf17097d644f8ec13752fc971
JrBour <calaruni@hotmail.com>
2020-11-09 20:54:19 +0100

    test(user): Test register method

--- tests/Controller/UsersTest.php
+++ tests/Controller/UsersTest.php
@@ -11,11 +11,34 @@ class UsersTest extends ApiTestCase
 
     public function testGetCollection(): void
     {
-        $response = static::createClient()->request('GET', '/users');
+        static::createClient()->request('GET', '/users');
         $this->assertResponseIsSuccessful();
         $this->assertResponseHeaderSame('content-type', 'application/json');
         $this->assertMatchesResourceCollectionJsonSchema(User::class);
+    }
 
+    public function testCreateUser(): void
+    {
+        $response = static::createClient()->request('POST', '/register', ['json' => [
+            'givenName' => 'Margaret',
+            'lastName' => 'Tutcher',
+            'phone' => '0123456789',
+            'email' => 'jo@Jo.com',
+            'password' => 'passwordz12',
+        ]]);
+
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertResponseHeaderSame('content-type', 'application/json');
+        $this->assertJsonContains([
+            'givenName' => 'Margaret',
+            'lastName' => 'Tutcher',
+            'phone' => '0123456789',
+            'email' => 'jo@Jo.com',
+            'password' => 'passwordz12',
+        ]);
+
+        $this->assertRegExp('~^/users/\d+$~', $response->toArray()['id']);
+        $this->assertMatchesResourceItemJsonSchema(User::class);
     }
 
 }
