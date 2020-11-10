d173cf013ee43f20e50581dd4c8799c178a0bce3
JrBour <calaruni@hotmail.com>
2020-11-10 21:12:24 +0100

    test(user): Add method for test the update on an user

--- tests/Controller/UsersTest.php
+++ tests/Controller/UsersTest.php
@@ -41,4 +41,28 @@ class UsersTest extends ApiTestCase
         $this->assertMatchesResourceItemJsonSchema(User::class);
     }
 
+    public function testUpdateUser(): void
+    {
+        $response = static::createClient()->request('PATCH', '/users/1', ['json' => [
+            'givenName' => 'Margaret',
+            'lastName' => 'Tutcher',
+            'phone' => '0123456789',
+            'email' => 'jo@Jo.com',
+            'password' => 'passwordz12',
+        ]]);
+
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseHeaderSame('content-type', 'application/json');
+        $this->assertJsonContains([
+            'givenName' => 'Margaret',
+            'lastName' => 'Tutcher',
+            'phone' => '0123456789',
+            'email' => 'jo@Jo.com',
+            'password' => 'passwordz12',
+        ]);
+
+        $this->assertRegExp('~^/users/\d+$~', $response->toArray()['id']);
+        $this->assertMatchesResourceItemJsonSchema(User::class);
+    }
+
 }
